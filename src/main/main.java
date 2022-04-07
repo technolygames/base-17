dd6bb2d9363f0cfa6a9cf1cb306d505cdd118e83
technolygames <69221500+technolygames@users.noreply.github.com>
2022-04-06 21:36:27 -0500

    direcciones en main.java arregladas

--- src/main/main.java
+++ src/main/main.java
@@ -6,14 +6,14 @@ import venPrimarias.start;
 public class main{
     public static void main(String[] args){
         new start().setVisible(true);
-        new makeDirs().makeDir(System.getProperty("user.dir")+"src/data/libs");
-        new makeDirs().makeDir(System.getProperty("user.dir")+"src/data/logs/static");
-        new makeDirs().makeDir(System.getProperty("user.dir")+"src/data/logs/exceptions");
-        new makeDirs().makeDir(System.getProperty("user.dir")+"src/data/config");
-        new makeDirs().makeDir(System.getProperty("user.dir")+"src/data/media/copy/label");
-        new makeDirs().makeDir(System.getProperty("user.dir")+"src/data/media/copy/icon");
-        new makeDirs().makeDir(System.getProperty("user.dir")+"src/data/media/forms");
-        new makeDirs().makeDir(System.getProperty("user.dir")+"src/data/media/secondary");
-        new makeDirs().makeDir(System.getProperty("user.dir")+"src/data/dataBackup");
+        new makeDirs().makeDir(System.getProperty("user.dir")+"/src/data/libs");
+        new makeDirs().makeDir(System.getProperty("user.dir")+"/src/data/logs/static");
+        new makeDirs().makeDir(System.getProperty("user.dir")+"/src/data/logs/exceptions");
+        new makeDirs().makeDir(System.getProperty("user.dir")+"/src/data/config");
+        new makeDirs().makeDir(System.getProperty("user.dir")+"/src/data/media/copy/label");
+        new makeDirs().makeDir(System.getProperty("user.dir")+"/src/data/media/copy/icon");
+        new makeDirs().makeDir(System.getProperty("user.dir")+"/src/data/media/forms");
+        new makeDirs().makeDir(System.getProperty("user.dir")+"/src/data/media/secondary");
+        new makeDirs().makeDir(System.getProperty("user.dir")+"/src/data/dataBackup");
     }
 }
